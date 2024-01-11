e97616bcc39226e6b67596e2e2de974bfb98158d
yasmeen <yasmeenhany14@gmail.com>
2024-01-11 19:31:45 +0200

    tasks from 0 to 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''This module contains function sum_list'''
+
+
+def sum_list(input_list: [float]) -> float:
+    '''Returns the sum of all floats in input_list'''
+    return sum(input_list)
