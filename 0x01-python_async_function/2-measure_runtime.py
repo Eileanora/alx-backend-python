fe522ce85c2a7616edc824225c5b31ea2a751688
yasmeen <yasmeenhany14@gmail.com>
2024-01-17 19:40:28 +0200

    remove the async annotation

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -7,10 +7,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     '''This function measures the total execution time for wait_n'''
-    start_time = time.time()
+    start_time: float = time.time()
     asyncio.run(wait_n(n, max_delay))
-    end_time = time.time()
-    total = end_time - start_time
-    return total / n
+    end_time: float = time.time()
+    return (end_time - start_time) / n
