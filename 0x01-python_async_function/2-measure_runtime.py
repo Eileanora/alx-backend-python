ca928b76724a1c58494f2e17f01285e34a2b58ef
yasmeen <yasmeenhany14@gmail.com>
2024-01-17 19:31:30 +0200

    fix how to run wait_n function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -11,6 +11,6 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 async def measure_time(n: int, max_delay: int) -> float:
     '''This function measures the total execution time for wait_n'''
     start_time = time.time()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end_time = time.time()
     return (end_time - start_time) / n
