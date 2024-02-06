69ebbbe64d8e96462c6efd6cc0c24fdb6d3fe6b6
yasmeen <yasmeenhany14@gmail.com>
2024-02-06 14:23:16 +0200

    fix typo

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,7 +9,7 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand(
         [
             ({"a": 1}, ["a"], 1),
-            ({"a": {"b": 2}}, ["a", "b"], 2)
+            ({"a": {"b": 2}}, ["a", "b"], 2),
             ({"a": {"b": 2}}, ["a"], {"b": 2})
         ]
     )
