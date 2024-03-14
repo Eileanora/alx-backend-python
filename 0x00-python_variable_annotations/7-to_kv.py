fee5f5080aa4296b3eef1af5e1bb35a7561e3c88
yasmeen <yasmeenhany14@gmail.com>
2024-03-15 00:58:51 +0200

    delete old tasks

--- 0x00-python_variable_annotations/7-to_kv.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-'''This module contains to_kv function'''
-from typing import Union, Tuple
-
-
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    '''Returns a tuple'''
-    return (k, v * v)
