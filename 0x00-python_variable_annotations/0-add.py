a2381bb076154ddecae32f12ef3df7e1dd8a361b
yasmeen <yasmeenhany14@gmail.com>
2024-03-15 01:00:10 +0200

    tasks

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''This module contains function add \
+that takes two floats and returns their sum as a float.'''
+
+
+def add(a: float, b: float) -> float:
+    '''Returns the sum of two floats as a float.'''
+    return (a + b)
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+'''This module contains function sum_list'''
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    '''Returns the sum of all floats in input_list'''
+    return sum(input_list)
